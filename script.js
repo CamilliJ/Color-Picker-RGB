9aa8a06f3ee1e1162d2b4546a350413358498b9e
Camilli Joannes <90011816+CamilliJ@users.noreply.github.com>
2022-04-17 18:26:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,95 @@
-// PreventDefault
-window.addEventListener('load',start)
+/*
+window.addEventListener('load', start);
 
-function start(){
-    console.log('carregada')
-    preventformSubmit()
+//Escopo global - Captura os elementos
+inputRed = document.querySelector('#inputRed');
+inputGreen = document.querySelector('#inputGreen');
+inputBlue = document.querySelector('#inputBlue');
+
+inputTxtRed = document.querySelector('#inputTxtRed');
+inputTxtGreen = document.querySelector('#inputTxtGreen');
+inputTxtBlue = document.querySelector('#inputTxtBlue');
+console.log(inputRed)
+var red = 0, green = 0, blue = 0;
+
+var divCor = document.querySelector('#aparece-cor');
+
+function start() {
+  preventFormSubmit();
 }
-function preventformSubmit(){
-    function handleSubmit(e){
-        e.preventDefault()
+
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
+  alterColor();
+
+}
+function alterColor() {
+  function handleChange(event) {
+    switch (event.target.id) {
+      case 'inputRed':
+        inputTxtRed.value = event.target.value;
+        red = event.target.value;
+        break;
+      case 'inputGreen':
+        inputTxtGreen.value = event.target.value;
+        green = event.target.value;
+        break;
+      default:
+        inputTxtBlue.value = event.target.value;
+        blue = event.target.value;
+        break;
     }
-    var form=document.querySelector('form')
-    form.addEventListener('submit',handleSubmit)
+    divCor.style.backgroundColor = `rgb(${red},${green},${blue})`
+  }
+  function startValues() {
+    inputRed.value = 0;
+    inputGreen.value = 0;
+    inputBlue.value = 0;
+
+    inputTxtRed.value = 0;
+    inputTxtGreen.value = 0;
+    inputTxtBlue.value = 0;
+
+    inputRed.addEventListener('change', handleChange);
+    inputGreen.addEventListener('change', handleChange);
+    inputBlue.addEventListener('change', handleChange);
+
+    divCor.style.backgroundColor = 'black'
+  }
+  startValues();
 }
 
 
-// function myColor(){
-//     var red = document.querySelector('#vermelho-rgb').value;
-//     var green = document.querySelector('#verde-rgb').value;
-//     var blue = document.querySelector('#azul-rgb').value;
 
-// }
+*/
 
 
+window.addEventListener('load', set)
 
+function set(){
+    var color = 'rgb(0,0,0)';
+}
 
+function myCor(){
+    var red = document.getElementById('inputRed').value;
+    var green = document.getElementById('inputGreen').value;
+    var blue = document.getElementById('inputBlue').value;
 
-// // Range
-// var RangeVermelho = document.querySelector('#vermelho-rgb');
-// var RangeVerde = document.querySelector('#verde-rgb');
-// var RangeAzul = document.querySelector('#azul-rgb');
+    var cor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 
-// // Inputs TXT
-// var InputVermelho = document.querySelector("#rgb-valor1");
-// var InputVerde = document.querySelector("#rgb-valor2");
-// var InputAzul = document.querySelector("#rgb-valor3");
+    document.body.style.backgroundColor = cor;
 
-// var vermelho = 0;
-// var verde = 0;
-// var azul = 0;
+    document.getElementById('inputTxtRed').value = red;
+    document.getElementById('inputTxtGreen').value = green;
+    document.getElementById('inputTxtBlue').value = blue;
+}
 
+document.getElementById('inputRed').addEventListener('input', myCor);
+document.getElementById('inputGreen').addEventListener('input', myCor);
+document.getElementById('inputBlue').addEventListener('input', myCor);
 
-// // Funcão AlteraCor() para agrupar as outras funções
-// function AlteraCor(){
-//     function Mudanca(event){
-//         switch (event.target.id){
-//              case 'vermelho-rgb':
-//                  InputVermelho.value = event.target.value;
-//                  vermelho = event.target.value;
-//              break;
-//              case 'verde-rgb':
-//                  InputVerde.value = event.target.value;
-//                  verde = event.target.value;
-//              break;
-//              default: 
-//              InputAzul.value = event.target.value;
-//              azul = event.target.value;
-//          }
-//          document.getElementById("aparece-cor").style.backgroundColor = 'rgb($vermelho},{$verde},{$azul}'
-//     }
-//     function StartValores(){
-//         RangeVermelho.value = 0;
-//         RangeVerde.value = 0;
-//         RangeAzul.value = 0;
-//         InputVermelho.value = 0;
-//         InputVerde.value = 0;
-//         InputAzul.value = 0;        
-//         InputVermelho.addEventListener('change', Mudanca )
-//         InputVerde.addEventListener('change', Mudanca )
-//         InputAzul.addEventListener('change', Mudanca)
-
-//        document.getElementById("aparece-cor").style.backgroundColor = "black";
-//     }
-//      StartValores();
-// } 
