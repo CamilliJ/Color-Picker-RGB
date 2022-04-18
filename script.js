cbedb95aa7e4da2fb82f9083152e8815614803be
Camilli Joannes <90011816+CamilliJ@users.noreply.github.com>
2022-04-18 15:26:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,34 @@
-/*
-window.addEventListener('load', start);
+// PreventDefault
+window.addEventListener('load',start)
 
-//Escopo global - Captura os elementos
-inputRed = document.querySelector('#inputRed');
-inputGreen = document.querySelector('#inputGreen');
-inputBlue = document.querySelector('#inputBlue');
-
-inputTxtRed = document.querySelector('#inputTxtRed');
-inputTxtGreen = document.querySelector('#inputTxtGreen');
-inputTxtBlue = document.querySelector('#inputTxtBlue');
-console.log(inputRed)
-var red = 0, green = 0, blue = 0;
-
-var divCor = document.querySelector('#aparece-cor');
-
-function start() {
-  preventFormSubmit();
+function start(){
+    console.log('carregada')
+    
 }
-
-function preventFormSubmit() {
-  function handleFormSubmit(event) {
-    event.preventDefault();
-  }
-
-  var form = document.querySelector('form');
-  form.addEventListener('submit', handleFormSubmit);
-  alterColor();
-
-}
-function alterColor() {
-  function handleChange(event) {
-    switch (event.target.id) {
-      case 'inputRed':
-        inputTxtRed.value = event.target.value;
-        red = event.target.value;
-        break;
-      case 'inputGreen':
-        inputTxtGreen.value = event.target.value;
-        green = event.target.value;
-        break;
-      default:
-        inputTxtBlue.value = event.target.value;
-        blue = event.target.value;
-        break;
+function preventformSubmit(){
+    function handleSubmit(e){
+        e.preventDefault()
     }
-    divCor.style.backgroundColor = `rgb(${red},${green},${blue})`
-  }
-  function startValues() {
-    inputRed.value = 0;
-    inputGreen.value = 0;
-    inputBlue.value = 0;
-
-    inputTxtRed.value = 0;
-    inputTxtGreen.value = 0;
-    inputTxtBlue.value = 0;
-
-    inputRed.addEventListener('change', handleChange);
-    inputGreen.addEventListener('change', handleChange);
-    inputBlue.addEventListener('change', handleChange);
-
-    divCor.style.backgroundColor = 'black'
-  }
-  startValues();
+    var form=document.querySelector('form')
+    form.addEventListener('submit',handleSubmit)
 }
 
-
-
-*/
 
 
 window.addEventListener('load', set)
 
 function set(){
-    var color = 'rgb(0,0,0)';
+    preventformSubmit()
+    var cor = 'rgb(0,0,0)';
+    document.getElementById('inputTxtRed').value = 255;
+    document.getElementById('inputTxtGreen').value = 255;
+    document.getElementById('inputTxtBlue').value = 255;
+
+    document.getElementById('inputRed').value = 255;
+    document.getElementById('inputGreen').value =255;
+    document.getElementById('inputBlue').value = 255;
+
+    
 }
 
 function myCor(){
@@ -80,13 +36,15 @@ function myCor(){
     var green = document.getElementById('inputGreen').value;
     var blue = document.getElementById('inputBlue').value;
 
-    var cor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    var cor = 'RGB(' + red + ',' + green + ',' + blue + ')';
 
     document.body.style.backgroundColor = cor;
 
     document.getElementById('inputTxtRed').value = red;
     document.getElementById('inputTxtGreen').value = green;
     document.getElementById('inputTxtBlue').value = blue;
+
+    document.getElementById('rgb-text').innerHTML = cor;
 }
 
 document.getElementById('inputRed').addEventListener('input', myCor);
